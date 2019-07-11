4733adc9113c203add6c2da2225a8ac138adfdfa
Jack Nugent <jnugent@captechventures.com>
2019-07-11 14:53:30 -0400

    add placeholder page links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
+import PageLinks from "../components/PageLinks"
 import SEO from "../components/seo"
 import { css } from "@emotion/core"
 
@@ -10,9 +11,22 @@ const image = css`
   max-width: 300px;
   margin-bottom: 1.45rem;
 `
+const links = css`
+  display: flex;
+  flex-wrap: nowrap;
+`
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+
+    <div css={links}>
+      <PageLinks />
+      <PageLinks />
+      <PageLinks />
+      <PageLinks />
+    </div>
+
     <h1>Hi people</h1>
     <p>Welcome to my blog.</p>
     <p>
