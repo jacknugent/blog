2de8f0726d8acc6857e9d39510fc77557717bd68
Jack Nugent <jnugent@captechventures.com>
2019-07-11 11:35:32 -0400

    make header use Emotion

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,28 +1,28 @@
 import { Link } from "gatsby"
 import * as React from "react"
+// this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core"
+
+const link = css`
+  color: white;
+  text-decoration: none;
+`
+const background = css`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`
+const text = css`
+  margin: 0 auto;
+  max-width: 960;
+  padding: 1.45rem 1.0875rem;
+`
 
 const Header = (props: HeaderProps) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+  <header css={background}>
+    <div css={text}>
+      <h1 css={{ margin: 0 }}>
+        <Link to="/" css={link}>
           {props.siteTitle}
         </Link>
       </h1>
