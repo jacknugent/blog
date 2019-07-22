8c22e0d44efefc437e5fd3e6c13abf29eaa26687
Jack Nugent <jnugent@captechventures.com>
2019-07-22 12:29:52 -0400

    youtube play component

---
+++ src/components/YouTubeVideo.tsx
@@ -0,0 +1,79 @@
+import * as React from "react"
+import { css } from "@emotion/core"
+import { useState } from "react"
+import youtube_red from "../utils/youtube-play-red.png"
+import youtube_grey from "../utils/youtube-play-grey.png"
+
+const YouTubeVideo = (props: YouTubeVideoProps) => {
+  const iframeContainer = css`
+    position: relative;
+    width: 100%;
+    padding-top: 56.25%; /* 16:9 Aspect Ratio */
+  `
+  const iframe = css`
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    border: 0;
+    z-index: 0;
+    background: url(${youtube_red}) center center no-repeat,
+      url(${`https://img.youtube.com/vi/` + props.id + `/maxresdefault.jpg`})
+        center center no-repeat;
+
+    background-size: 100px, cover;
+  `
+
+  const youtubeVideo = css`
+    padding-bottom: 2rem;
+  `
+
+  const loadingThumbnail = css`
+    background: url(${youtube_grey}) center center no-repeat,
+      url(${`https://img.youtube.com/vi/` + props.id + `/maxresdefault.jpg`})
+        center center no-repeat;
+    background-size: 100px, cover;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100%;
+    width: 100%;
+    z-index: 1;
+    &:hover {
+      cursor: pointer;
+      background: url(${youtube_red}) center center no-repeat,
+        url(${`https://img.youtube.com/vi/` + props.id + `/maxresdefault.jpg`})
+          center center no-repeat;
+      background-size: 100px, cover;
+    }
+  `
+  const [isVideo, setIsVideo] = useState(false)
+
+  return (
+    <div css={youtubeVideo}>
+      <div css={iframeContainer} onClick={x => setIsVideo(true)}>
+        {isVideo ? (
+          <iframe
+            css={iframe}
+            src={
+              "https://www.youtube.com/embed/" +
+              props.id +
+              "?rel=0&autoplay=1&mute=0"
+            }
+          />
+        ) : (
+          <div css={loadingThumbnail} />
+        )}
+      </div>
+    </div>
+  )
+}
+
+interface YouTubeVideoProps {
+  id: string
+}
+
+export default YouTubeVideo
--- src/pages/stripes.tsx
+++ src/pages/stripes.tsx
@@ -1,52 +1,12 @@
 import * as React from "react"
 import { useState } from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import { css } from "@emotion/core"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import background from "../utils/stripe-gallery/juno-background.jpg"
 import ImageGallery from "../components/ImageGallery"
 import StripeSearch from "../components/StripeSearch"
-
-const iframeContainer = css`
-  position: relative;
-  width: 100%;
-  padding-top: 56.25%; /* 16:9 Aspect Ratio */
-`
-const iframe = css`
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  border: 0;
-  z-index: 0;
-  background: url(${background}) center center no-repeat;
-  background-size: cover;
-`
-
-const youtubeVideo = css`
-  padding-bottom: 2rem;
-`
-
-const loadingThumbnail = css`
-  background: url(${background}) center center no-repeat;
-  background-size: cover;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 100%;
-  width: 100%;
-  z-index: 1;
-  &:hover {
-    cursor: pointer;
-  }
-`
+import YouTubeVideo from "../components/YouTubeVideo"
 
 function stripes() {
-  const [isVideo, setIsVideo] = useState(false)
   const [search, setSearch] = useState("")
   const [filter, setFilter] = useState("All")
 
@@ -55,18 +15,7 @@ function stripes() {
       <Layout>
         <SEO title="Stripes" />
         <h1>Stripes in Film</h1>
-        <div css={youtubeVideo}>
-          <div css={iframeContainer} onClick={x => setIsVideo(true)}>
-            {isVideo ? (
-              <iframe
-                css={iframe}
-                src="https://www.youtube.com/embed/Y1U4YkNkoG0?rel=0&autoplay=1&mute=0"
-              />
-            ) : (
-              <div css={loadingThumbnail} />
-            )}
-          </div>
-        </div>
+        <YouTubeVideo id="Y1U4YkNkoG0"></YouTubeVideo>
         <StripeSearch
           searchValue={search}
           search={(e: any) => setSearch(e.target.value)}
