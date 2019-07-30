4d4f29bb016427decd57e7691b98f942c941600e
Jack Nugent <jnugent@captechventures.com>
2019-07-30 16:15:06 -0400

    added robots.txt configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,6 +64,14 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: "https://jacknugent.io",
+        sitemap: "https://jacknugent.io/sitemap.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
