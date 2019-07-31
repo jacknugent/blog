2874cb0b300af90113de40bf2aae9a893fe4cc3c
Jack Nugent <jnugent@captechventures.com>
2019-07-31 11:36:33 -0400

    working on video gallery

--- src/pages/videos.tsx
+++ src/pages/videos.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { useState, useRef, useEffect } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,6 +7,8 @@ import { css } from "@emotion/core"
 import YouTubeVideo from "../components/YouTubeVideo"
 
 function videos() {
+  const [clickedVideo, setClickedVideo] = useState(null)
+
   const videoInfo = useStaticQuery(graphql`
     {
       allYoutubeVideo {
@@ -26,27 +29,42 @@ function videos() {
   `).allYoutubeVideo.edges
 
   const youtubeVideoGallery = css`
-    display: flex;
-    flex-wrap: wrap;
-    margin-left: calc(-100vw / 2 + 960px / 2 + 0.5rem);
-    margin-right: calc(-100vw / 2 + 960px / 2 + 0.5rem);
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-column-gap: 0.5rem;
+    grid-row-gap: 0.5rem;
+    margin-left: calc(-100vw / 2 + 960px / 2 + 1rem);
+    margin-right: calc(-100vw / 2 + 960px / 2 + 1rem);
+
+    @media (max-width: 1200px) {
+      grid-template-columns: repeat(2, 1fr);
+    }
     @media (max-width: 960px) {
+      grid-template-columns: repeat(1, 1fr);
       margin-left: -1rem;
       margin-right: -1rem;
     }
   `
-  const videoContainer = css`
-    margin: 0.5rem;
-    flex-grow: 1;
-    width: 25%;
+  const videoContainer = css``
 
-    @media (max-width: 1200px) {
-      width: 33%;
-    }
-    @media (max-width: 960px) {
-      width: 100%;
-    }
-  `
+  const handleIFrameClick = (e: any, i: number) => {
+    const video_width = window.innerWidth / 3 - 18
+    const aspect_ratio = 16 / 9
+    const video_height = video_width / aspect_ratio
+    const row = Math.floor(i / 3)
+    const first_in_row = Number.isInteger(i / 3) ? 0 : 1
+
+    console.log("row", row)
+    console.log(first_in_row, "first_in_row")
+    const expanded_video = video_height * 2 + 9
+
+    setClickedVideo(i)
+    window.scroll({
+      top: 98 + (row + first_in_row) * (video_height + 9),
+      left: 0,
+      behavior: "smooth",
+    })
+  }
 
   return (
     <Layout>
@@ -57,15 +75,43 @@ function videos() {
       >
         <SEO title="Videos" />
         <div css={youtubeVideoGallery}>
-          {videoInfo.map((x: any, i: number) => (
-            <div css={videoContainer} onClick={() => console.log("helo")}>
+          {videoInfo.map((x: any, i: number) => {
+            const video = (
               <YouTubeVideo
                 key={"youtube_video_" + i}
                 id={x.node.videoId}
                 fluid_url={x.node.localThumbnail.childImageSharp.fluid}
               ></YouTubeVideo>
-            </div>
-          ))}
+            )
+
+            return i === clickedVideo ? (
+              <div
+                css={[
+                  videoContainer,
+                  css`
+                    grid-column: 1 / 4;
+                    grid-row: ${i / 4} / ${i / 3 + 4};
+                    // -webkit-transition: width 0.5s; /* Safari prior 6.1 */
+                    // transition: width 0.5s;
+                    -webkit-transition: max-width 0.5s; /* Safari prior 6.1 */
+                    transition: max-width 0.5s;
+                  `,
+                ]}
+                onClick={() => {}}
+              >
+                {video}
+              </div>
+            ) : (
+              <div
+                css={videoContainer}
+                onClick={e => {
+                  handleIFrameClick(e, i)
+                }}
+              >
+                {video}
+              </div>
+            )
+          })}
         </div>
       </div>
     </Layout>
