ec1dfa963e8ea5fdb84d2b9e326689d154a21089
Jack Nugent <jnugent@captechventures.com>
2019-08-26 15:07:39 -0400

    working Wordpress Gatsby site

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,6 +58,21 @@ module.exports = {
         policy: [{ userAgent: "*", allow: "/" }],
       },
     },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        baseUrl: `live-poc-see-kids-dream.pantheonsite.io`,
+        // WP.com sites set to true, WP.org set to false
+        hostingWPCOM: false,
+        // The protocol. This can be http or https.
+        protocol: "http",
+        // Use 'Advanced Custom Fields' Wordpress plugin
+        useACF: false,
+        auth: {},
+        // Set to true to debug endpoints on 'gatsby build'
+        verboseOutput: false,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
