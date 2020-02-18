1eb14ca27e44c3757efcc6e551daaf6c12dc3b54
Jack Nugent <jnugent@captechconsulting.com>
2020-02-18 15:14:20 -0500

    icons hack

--- src/pages/icons.js
+++ src/pages/icons.js
@@ -1,7 +1,36 @@
 import * as React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import styled from "@emotion/styled";
 
-const icons = () => {
-  return <div>icons!!</div>;
+const Icons = () => {
+  const youtube_videos = useStaticQuery(graphql`
+    {
+      videos: allYoutubeVideo {
+        nodes {
+          videoId
+          title
+          localThumbnail {
+            publicURL
+          }
+        }
+      }
+    }
+  `).videos.nodes;
+
+  const Links = styled.a`
+    display: block;
+  `;
+
+  return (
+    <>
+      <Links href={"/icons/icon-144x144.png"}>NYSI Logo</Links>
+      {youtube_videos.map((video, i) => (
+        <Links key={i} href={video.localThumbnail.publicURL}>
+          {video.title}
+        </Links>
+      ))}
+    </>
+  );
 };
 
-export default icons;
+export default Icons;
