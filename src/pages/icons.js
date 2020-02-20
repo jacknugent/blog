39de042b6be67b379dbd6357f1e7bf863fc3e96e
Jack Nugent <jnugent@captechconsulting.com>
2020-02-19 20:03:19 -0500

    style icons page

--- src/pages/icons.js
+++ src/pages/icons.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import styled from "@emotion/styled";
+import image_icon from "../utils/media/images/youtube_mash/index_icon.gif";
 
 const Icons = () => {
   const youtube_videos = useStaticQuery(graphql`
@@ -9,6 +10,7 @@ const Icons = () => {
         nodes {
           videoId
           title
+          publishedAt
           localThumbnail {
             publicURL
           }
@@ -23,13 +25,35 @@ const Icons = () => {
 
   return (
     <>
-      <h1>Index of /icons</h1>
-      <Links href={"/icons/icon-144x144.png"}>NYSI Logo</Links>
-      {youtube_videos.map((video, i) => (
-        <Links key={i} href={video.localThumbnail.publicURL}>
-          {video.title}
-        </Links>
-      ))}
+      <h1>Index of /icons/</h1>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Publish Date</th>
+          </tr>
+          <tr>
+            <th colspan={"5"}>
+              <hr></hr>
+            </th>
+          </tr>
+
+          {youtube_videos.map((video, i) => (
+            <tr>
+              <td>
+                <img src={image_icon} alt="[IMG]"></img>
+              </td>
+              <td>
+                <Links key={i} href={video.localThumbnail.publicURL}>
+                  {video.title}
+                </Links>
+              </td>
+              <td>{video.publishedAt.slice(0, 10)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
